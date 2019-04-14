dc132e2fe88696d746c8302d7e7af206e1df5742
seedship☄️ <rnai2@illinois.edu>
2019-04-13 21:46:33 -0500

    all implementation steps done

--- MP3/mp3.c
+++ MP3/mp3.c
@@ -12,6 +12,9 @@
 #include <linux/kthread.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/mm.h>
+
+#include <asm/page_types.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Group_ID");
@@ -33,6 +36,7 @@ typedef struct mp3_task_struct_t {
 		unsigned long stime;
 		unsigned long major_fault;
 		unsigned long minor_fault;
+		pid_t pid;
 		struct list_head task_node;
 } mp3_task_struct;
 
@@ -45,7 +49,7 @@ static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 
 //vbuffer
-static unsigned long *vbuffer;
+static uint8_t *vbuffer;
 
 //workqueue things
 static struct workqueue_struct *queue;
@@ -74,7 +78,7 @@ mp3_task_struct * find_mp3_task_struct_by_pid(pid_t pid){
 	mutex_lock(&task_struct_mutex);
 	list_for_each(pos, &head.task_node){
 		mp3_task_struct * tmp = list_entry(pos, mp3_task_struct, task_node);
-		if(tmp->linux_task->pid == pid){
+		if(tmp->pid == pid){
 			mutex_unlock(&task_struct_mutex);
 			return tmp;
 		}
@@ -89,30 +93,40 @@ DECLARE_DELAYED_WORK(mp3_deplayed_work, mp3_work_func);
 
 static void mp3_work_func(struct work_struct *work){
 	struct list_head *pos;
+	struct list_head *q;
 
 	unsigned long total_major_fault = 0;
 	unsigned long total_minor_fault = 0;
 	unsigned long total_utilization = 0;
 
-	mutex_lock(&task_struct_mutex);
-	list_for_each(pos, &head.task_node){
+	if(!num_entries){
+		vbuffer_idx = 0;
+		return;
+	}
+
+	list_for_each_safe(pos, q, &head.task_node){
 		mp3_task_struct * tmp = list_entry(pos, mp3_task_struct, task_node);
 		//If task valid, update use. Otherwise, remove from list.
-		if(!get_cpu_use(tmp->linux_task->pid, &tmp->minor_fault, &tmp->major_fault, &tmp->utime, &tmp->stime)){
+		if(!get_cpu_use(tmp->pid, &tmp->minor_fault, &tmp->major_fault, &tmp->utime, &tmp->stime)){
 			total_major_fault += tmp->major_fault;
 			total_minor_fault += tmp->minor_fault;
 			total_utilization += (tmp->utime + tmp->stime);
 		} else {
-			printk(KERN_ALERT "Can not find cpu use for unknown pid: %u\n", tmp->linux_task->pid);
+			printk(KERN_ALERT "Can not find CPU use for unknown PID: %u, deleting it\n", tmp->pid);
+			list_del(pos);
+			kfree(tmp);
+			num_entries--;
 		}
 	}
 	mutex_unlock(&task_struct_mutex);
 
-	vbuffer[vbuffer_idx++] = jiffies;
-	vbuffer[vbuffer_idx++] = total_minor_fault;
-	vbuffer[vbuffer_idx++] = total_major_fault;
-	vbuffer[vbuffer_idx++] = total_utilization;
+	((unsigned long*)vbuffer)[vbuffer_idx++] = jiffies;
+	((unsigned long*)vbuffer)[vbuffer_idx++] = total_minor_fault;
+	((unsigned long*)vbuffer)[vbuffer_idx++] = total_major_fault;
+	((unsigned long*)vbuffer)[vbuffer_idx++] = total_utilization;
 	vbuffer_idx %= (VBUFFER_SIZE_B / sizeof(unsigned long));
+
+	printk(KERN_ALERT "vbuffer idx: %u max idx: %lu\n", vbuffer_idx, VBUFFER_SIZE_B / sizeof(unsigned long));
 
 	queue_delayed_work(queue, &mp3_deplayed_work, msecs_to_jiffies(50));
 }
@@ -140,7 +154,7 @@ static ssize_t mp3_read(struct file *file, char __user *buffer, size_t count, lo
 		mutex_lock(&task_struct_mutex);
 		list_for_each(pos, &head.task_node){
 			mp3_task_struct * tmp = list_entry(pos, mp3_task_struct, task_node);
-			offset += sprintf(kernelbuffer + offset, "PID:\t%u\n", tmp->linux_task->pid);
+			offset += sprintf(kernelbuffer + offset, "PID:\t%u\n", tmp->pid);
 		}
 		mutex_unlock(&task_struct_mutex);
 
@@ -174,6 +188,8 @@ static ssize_t mp3_write(struct file *file, const char *buffer, size_t count, lo
 	pid_t pid;
 	mp3_task_struct* tmp;
 
+	struct task_struct *toremove;
+
 	//calculate buffer size
 	if ( count > PROCFS_MAX_SIZE )	{
 		procfs_buffer_size = PROCFS_MAX_SIZE;
@@ -190,36 +206,41 @@ static ssize_t mp3_write(struct file *file, const char *buffer, size_t count, lo
 	switch (procfs_buffer[0]) {
 		case 'R': //Registration
 			sscanf(&procfs_buffer[2], "%u", &pid);
+			printk(KERN_ALERT "Registering task with PID:%u\n", pid);
 			tmp = (mp3_task_struct*)kmalloc(sizeof(mp3_task_struct), GFP_KERNEL);
 			tmp->linux_task = find_task_by_pid(pid);
 			tmp->major_fault = 0;
 			tmp->minor_fault = 0;
+			tmp->pid = pid;
 			tmp->utime = 0;
 			tmp->stime = 0;
 			INIT_LIST_HEAD(&tmp->task_node);
 
-			if(!num_entries) queue_delayed_work(queue, &mp3_deplayed_work, msecs_to_jiffies(50));
-
 			mutex_lock(&task_struct_mutex);
+			if(!num_entries) queue_delayed_work(queue, &mp3_deplayed_work, msecs_to_jiffies(50));
 			num_entries++;
 			list_add(&(tmp->task_node), &head.task_node);
 			mutex_unlock(&task_struct_mutex);
+			printk(KERN_ALERT "Registered task with PID:%u\n", tmp->pid);
 			break;
 		case 'U': //Unregistration
 			sscanf(&procfs_buffer[2], "%u", &pid);
-
-			printk(KERN_ALERT "Begin unregistering task with pid: %u\n", pid);
-
+			printk(KERN_ALERT "Begin unregistering task with PID:%u\n", pid);
 			tmp = find_mp3_task_struct_by_pid(pid);
-			printk("%p\n", tmp);
-			mutex_lock(&task_struct_mutex);
-			list_del(&(tmp->task_node));
-			kfree(tmp);
-			mutex_unlock(&task_struct_mutex);
-			printk(KERN_ALERT "Finish unregistering task with pid: %u\n", pid);
+			if(tmp){
+				toremove = tmp->linux_task;
+				mutex_lock(&task_struct_mutex);
+				num_entries--;
+				list_del(&(tmp->task_node));
+				kfree(tmp);
+				mutex_unlock(&task_struct_mutex);
+				printk(KERN_ALERT "Unregistered task with PID:%u\n", pid);
+			} else {
+				printk(KERN_ALERT "Received unknown PID in unregister: %u\n", pid);
+			}
 			break;
 		default:
-			printk(KERN_ALERT "Received unknown prefix in m3_write\n");
+			printk(KERN_ALERT "Received unknown prefix in mp3_write\n");
 			break;
 	}
 
@@ -238,6 +259,19 @@ static const struct file_operations mp3_file = {
 
 static int chardev_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+	unsigned long x;
+	unsigned long pfn;
+
+	if(vma->vm_end - vma->vm_start > VBUFFER_SIZE_B) return -EINVAL;
+
+	for(x = 0; x < VBUFFER_SIZE_B; x+=PAGE_SIZE){
+		pfn = vmalloc_to_pfn((void*)(vbuffer+x));
+		if(remap_pfn_range(vma, vma->vm_start+x, pfn, PAGE_SIZE, vma->vm_page_prot)){
+			printk(KERN_ALERT "MP3 module could not perform mmap!\n");
+			return -EAGAIN;
+		}
+	}
+//	printk(KERN_ALERT "worked!\n");
 	return 0;
 }
 
@@ -257,16 +291,23 @@ static const struct file_operations mp3_chardev_fops = {
  */
 int __init mp3_init(void)
 {
+	unsigned long x;
 #ifdef DEBUG
 	printk(KERN_ALERT "MP3 MODULE LOADING\n");
 #endif
+
 	//create proc files
 	proc_dir = proc_mkdir("mp3", NULL);
 	proc_entry = proc_create("status", 0666, proc_dir, &mp3_file);
 
 	//create vbuffer
 	vbuffer = vmalloc(VBUFFER_SIZE_B);
+	memset(vbuffer, -1, VBUFFER_SIZE_B);
 	vbuffer_idx = 0;
+
+	for(x = 0; x < VBUFFER_SIZE_B; x+=PAGE_SIZE){
+		SetPageReserved(vmalloc_to_page((void*)(vbuffer+x)));
+	}
 
 	//create a mutex
 	mutex_init(&task_struct_mutex);
@@ -279,6 +320,8 @@ int __init mp3_init(void)
 	//init list
 	num_entries = 0;
 	INIT_LIST_HEAD(&head.task_node);
+
+//	printk(KERN_ALERT "Page size is %lu\n", PAGE_SIZE);
 
 	//create workqueue and timer
 	queue = create_singlethread_workqueue("mp3_workqueue");
@@ -294,6 +337,7 @@ void __exit mp3_exit(void)
 {
 	struct list_head *pos, *q;
 	mp3_task_struct* tmp;
+	unsigned long x = 0;
 #ifdef DEBUG
 	printk(KERN_ALERT "MP3 MODULE UNLOADING\n");
 #endif
@@ -312,7 +356,7 @@ void __exit mp3_exit(void)
 	list_for_each_safe(pos, q, &head.task_node){
 		tmp = list_entry(pos, mp3_task_struct, task_node);
 		list_del(pos);
-		vfree(tmp);
+		kfree(tmp);
 	}
 	mutex_unlock(&task_struct_mutex);
 
@@ -323,6 +367,9 @@ void __exit mp3_exit(void)
 	unregister_chrdev_region(mp3_dev, 1);
 
 	//remove memory buffer
+	for(x = 0; x < VBUFFER_SIZE_B; x+=PAGE_SIZE){
+		ClearPageReserved(vmalloc_to_page((void*)(vbuffer+x)));
+	}
 	vfree(vbuffer);
 
 	printk(KERN_ALERT "MP3 MODULE UNLOADED\n");
